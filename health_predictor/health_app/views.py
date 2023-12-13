dc85501f488ea88594de2ffd81631f3c16d031f2
ADITHYA S NAIR <adithyasnair2021@gmail.com>
2023-12-13 10:00:40 +0530

    Alzhiemers
    alzhiemers done

--- health_predictor/health_app/views.py
+++ health_predictor/health_app/views.py
@@ -3,12 +3,16 @@ from django.http import HttpResponse
 import uuid,random
 from django.contrib.auth.decorators import login_required
 from datetime import datetime
-#from .forms import UserForm,events_create
-#from .models import user_details,events
 from django.contrib.auth.models import User,auth
 from django.contrib import messages
 from itertools import chain
+from gradio_client import Client
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    client = Client("https://adithyasnair-alzheimers-prediction-using-cnn.hf.space/")
+    result = client.predict(
+				"D:/Sem 5/Software/Software_engineering/health_predictor/ml_models/alzheimers/Non(1).jpg",	# str (filepath or URL to image) in 'image' Image component
+				api_name="/predict")
+    print(result)
+    return render(request,'index.html',{'result':result})
